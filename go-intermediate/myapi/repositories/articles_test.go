2ef67b2e7c8e4521fe58a351dbae2349873667ae
Ohmiya-Mizuki <m.omiya@miwaza.co.jp>
2024-10-15 09:48:01 +0900

    テストコードのリファクタ

--- go-intermediate/myapi/repositories/articles_test.go
+++ go-intermediate/myapi/repositories/articles_test.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/yourname/reponame/models"
 	"github.com/yourname/reponame/repositories"
+	"github.com/yourname/reponame/repositories/testdata"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 // SelectArticleList関数のテスト
 func TestSelectArticleList(t *testing.T) {
-	expectedNum := 4
+	expectedNum := len(testdata.ArticleTestData)
 	got, err := repositories.SelectArticleList(testDB, 1)
 	if err != nil {
 		t.Fatal(err)
@@ -30,22 +31,10 @@ func TestSelectArticleDetail(t *testing.T) {
 	}{
 		{
 			testTitle: "subtest1",
-			expected: models.Article{
-				ID:       1,
-				Title:    "firstPost",
-				Contents: "This is my first blog",
-				UserName: "saki",
-				NiceNum:  3,
-			},
+			expected: testdata.ArticleTestData[0],
 		}, {
 				testTitle: "subtest2",
-				expected: models.Article{
-					ID:       2,
-					Title:    "2nd",
-					Contents: "Second blog post",
-					UserName: "saki",
-					NiceNum:  4,
-				},
+				expected: testdata.ArticleTestData[1],
 		},
 	}
 	
